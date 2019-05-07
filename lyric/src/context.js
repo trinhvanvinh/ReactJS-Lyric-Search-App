cee2150508693ba680c35d90604ddd29002690c8
trinhvanvinh <trinhvanvinh018@gmail.com>
2019-05-08 00:03:37 +0700

    no message

--- lyric/src/context.js
+++ lyric/src/context.js
@@ -1,18 +1,31 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 const Context = React.createContext();
+const reducer =(state, action)=>{
+    switch(action.type){
+        case'SEARCH_TRACKS':
+            return {
+                ...state,
+                track_list:action.payload,
+                heading:'Search Result'
+            };
+            default:
+            return state;
+    }
+}
 
 export class Provider extends Component{
     state={
         track_list:[],
-        heading:'Top 10 Tracks'
+        heading:'Top 10 Tracks',
+        dispatch:action=>this.setState(state=>reducer(state, action))
     }
 
     componentDidMount(){
         console.log("didmount");
 
         axios.get('https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/'
-        +'chart.tracks.get?page=1&page_size=10&country=vn&f_has_lyrics=1'
+        +'chart.tracks.get?page=1&page_size=20&country=vn&f_has_lyrics=1'
         +'&apikey='
         +'10a2d4832ec518f86205fb735be374e1'
         )
